dfa0ae0505a898f6c2213124240d57ee03307c58
Rodrigo Brunelli Riguetto <rbriguetto@gmail.com>
2024-02-25 15:24:02 -0300

    feat(backend): Serving index.html for 404 non api endpoints.

--- src/Notes.Api/Program.cs
+++ src/Notes.Api/Program.cs
@@ -17,6 +17,30 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseDefaultFiles();
+app.UseStaticFiles();
 app.MapControllers();
+
+app.Use(async (context, next) => {
+    await next();
+
+    // Serving index.html for 404 non api endpoints. Necessary
+    // for angular routing
+
+    if (context.Response.StatusCode != 404 || context.Request.Path.Value!.Contains("api/")) 
+    {
+        return;
+    }
+
+    string indexFileName = Path.Combine("wwwroot", "index.html");
+    if (!File.Exists(indexFileName)) 
+    {
+        await context.Response.WriteAsync($"spaclient not found on wwwroot");
+        return;
+    }
+    string text = File.ReadAllText(indexFileName);
+    context.Response.StatusCode = 200;
+    await context.Response.WriteAsync(text);
+});
 
 app.Run();
